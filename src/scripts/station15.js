7c7a954c9df8b145654915fabead817797649215
Yutaro Hara <77527605+aGO-MakeSomething@users.noreply.github.com>
2024-10-07 12:50:00 +0900

    [feat] Complete station15.

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+async function test(userList) {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 3000)
+  })
+  return await promise.then(() => userList.map(buildFullName))
 }
 
+function buildFullName(data) {
+  const full_name = `${data.family_name} ${data.first_name}`;
+  return { id: data.id, full_name, first_name: data.first_name, family_name: data.family_name, affiliation: data.affiliation, is_student: data.is_student }
+}
